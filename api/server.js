6256814de2152c7ccd5bfa44339d39e9e254dc18
BunniMK <86545296+BunniMK@users.noreply.github.com>
2022-01-10 16:58:55 -0700

    Added delete

--- api/server.js
+++ api/server.js
@@ -32,10 +32,6 @@ server.get('/api/users', async (req, res) => {
 })
 
 server.get('/api/users/:id', async (req, res) => {
-    console.log(req.method)
-    console.log(req.headers)
-    console.log(req.body)
-    console.log(req.params)
     try{
         const user = await User.findById(req.params.id)
         if(!user) {
@@ -43,6 +39,21 @@ server.get('/api/users/:id', async (req, res) => {
         } else {
             res.status(200).json(user)
         }  
+    }
+    catch (err){
+        res.status(500).json({ message: err.message })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try{
+        const { id } = req.params
+        const deletedUser = await User.remove(id)
+        if(!deletedUser){
+            res.status(404).json({ message: 'There is no user by that id!'})
+        } else{
+            res.json(deletedUser)
+        }
     }
     catch (err){
         res.status(500).json({ message: err.message })
